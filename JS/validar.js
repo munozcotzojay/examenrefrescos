e02ea084974233487358089698036791c9842067
munozcotzojay <91991291+munozcotzojay@users.noreply.github.com>
2021-10-14 22:26:32 -0700

    Add files via upload

---
+++ JS/validar.js
@@ -0,0 +1,12 @@
+function go()
+{
+
+if (document.formingreso.usuario.value=='lisy' && document.formingreso.contraseña.value=='examen')
+    {
+    window.location="html/inicio.html"
+    }
+else 
+    {
+    alert("Ingrese su usuario y contraseña correctamente.");
+    }
+}
